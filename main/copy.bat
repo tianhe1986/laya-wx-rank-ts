3c11bb142c5d11bab087a91079ed3c86a730e912
tianhe1986 <w1s2j3229@163.com>
2018-10-01 00:24:50 +0800

    去掉注释。
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- main/copy.bat
+++ main/copy.bat
@@ -1,4 +1,3 @@
-:: 拷贝rank
 cd /d %~dp0
 mkdir bin\rankRes
 copy bin\res\atlas\rank.atlas bin\rankRes\rank.atlas /Y
--- main/release.bat
+++ main/release.bat
@@ -2,15 +2,12 @@ cd /d %~dp0
 set binDir=bin
 set srcDir=release\wxgame
 set dstDir=..\wxgame
-::删除小游戏用不到的目录
+
 del /f /s /q %srcDir%\libs\*.*
 rd /s /q %srcDir%\libs
 del /f /s /q %srcDir%\js\*.*
 rd /s /q %srcDir%\js
-:: 拷贝rank资源，如果有其他需要的资源也可以一并拷贝
+
 Xcopy %binDir%\rankRes %srcDir%\rankRes /s /e /y
-:: 拷贝到真正发布的目录
-::del /f /s /q %dstDir%\*.*
-::rd /s /q %dstDir%
-::mkdir %dstDir%
+
 Xcopy %srcDir% %dstDir% /s /e /y
